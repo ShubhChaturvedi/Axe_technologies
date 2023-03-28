2fdeb15120140efd227a5f1d478344120c91927d
ShubhChaturvedi <chaturvedishubh650@gmail.com>
2023-03-28 13:02:50 +0530

    DEV: complete

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+import csv
 
 driver = webdriver.Chrome(executable_path="/home/shubh/Downloads/chromedriver_linux64.exe")
 driver.maximize_window()
@@ -15,21 +16,32 @@ query.send_keys(part_number)
 time.sleep(1)
 query.send_keys(Keys.ENTER)
 query2 = driver.find_element(by=By.XPATH, value='//*[@id="rso"]/div[1]/div/div/div[1]/div/div/div[2]/div/span/em').text
-print(query2)
+
 
 js = """
+        let links = []
         for(let i=0;i<document.getElementsByClassName('lyLwlc').length;i++){
-            let divs = document.getElementsByClassName('lyLwlc')[i]
-            let spans = divs.getElementsByTagName("span")[0]
+            let divs = document.getElementsByClassName('lyLwlc')[i];
+            let spans = divs.getElementsByTagName("span")[0];
             if(spans != undefined){
                 if(spans.getElementsByTagName("em")[0].innerHTML === "90NR03U2" ){
-                    console.log("sahi h tu")
+                    let a = document.getElementsByClassName('yuRUbf')[i].getElementsByTagName("a")[0].getAttribute("href");
+                    let link = [a];
+                    links.push(link);
                 }
             }
         }
+        return links;
     """
-driver.execute_script(js)
-# if(query2 == ):
-time.sleep(100)
+links = driver.execute_script(js)
+with open('output.csv', mode='a', newline='') as file:
+    # Create a writer object
+    writer = csv.writer(file)
+
+    # Write the data to the CSV file
+    for link in links:
+        writer.writerow(link)
+
+
 
 driver.close()
