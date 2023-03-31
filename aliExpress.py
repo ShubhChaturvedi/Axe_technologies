2b96119fc4ea9105bf9926bedd44250efa75ac33
ShubhChaturvedi <chaturvedishubh650@gmail.com>
2023-03-31 10:54:27 +0530

    DEV: Aliexpress

--- aliExpress.py
+++ aliExpress.py
@@ -9,21 +9,46 @@ driver.maximize_window()
 
 driver.get("https://www.aliexpress.com")
 time.sleep(1)
-# query = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input")
-# search = "aliexpress"
-# query.send_keys(search)
-#
-# time.sleep(1)
-# query.send_keys(Keys.ENTER)
-# query2 = driver.find_element(by=By.XPATH, value='//*[@id="rso"]/div[1]/div/div/div/div/div/div/div/div[1]/a').click()
-# time.sleep(10)
-#
-# js = """
-#         let data = {};
-#
-#     """
-# links = driver.execute_script(js)
+query = driver.find_element(by=By.XPATH, value='//*[@id="search-key"]')
+search = 'smartphone'
+query.send_keys(search)
+time.sleep(1)
+query.send_keys(Keys.ENTER)
+js = """
+    let data = {
+        "source_name": "aliexpress"
+    };
+    let content = document.getElementsByClassName('manhattan--container--1lP57Ag cards--gallery--2o6yJVt');
+    for(let i=0;i<content.length;i++){
+        content[i].click();
+        let smaple = {
+            "product_title": "",
+            "product_detail_page_url": content[i].getAttribute("href"),
+            "product_brand": "",
+            "product_features": "",
+            "product_part_number": "",
+            "product_model_number": "",
+            "product_price": "",
+            "product_display_price": "",
+            "product_currency": "",
+            "category_id": "",
+            "product_source_id": "",
+            "product_specifications": "",
+            "is_active": "",
+            "product_image_url": "",
+            "product_image_type": "",
+            "product_image_type_size": "",
+            "product_image_height": "",
+            "product_image_width": "",
+            
+            };
+            data.content[i] = smaple;
+        };
+    }
+"""
 
-
+data = driver.execute_script(js)
+print(data)
+time.sleep(20)
 
 driver.close()
