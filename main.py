cba8532bfd8ada9aa5b911a2710af3e6ac5ece26
ShubhChaturvedi <chaturvedishubh650@gmail.com>
2023-04-05 16:55:00 +0530

    DEV: test on data

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
+import app
+import csv
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    with open('app_product_data.csv', mode='r') as file:
+        reader = csv.reader(file)
+        platforms = ['amazon', 'flipkart', 'reliancedigital', 'croma', 'vijaysales', 'samsung', 'hp', 'redmi', 'mi',
+                     'nykaa', 'myntra']
+        for row in reader:
+            part_number = row[2]
+            product_title = row[1]
+            app.get_links(part_number, platforms, product_title)
