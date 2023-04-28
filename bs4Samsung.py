3f00a437fbc1797a44fd0e45f220604cf504cf3c
ShubhChaturvedi <chaturvedishubh650@gmail.com>
2023-04-28 14:14:47 +0530

    OUT: updated

--- bs4Samsung.py
+++ bs4Samsung.py
@@ -4,10 +4,11 @@ def findPrice():
     url = "https://www.samsung.com/in/smartphones/galaxy-m/galaxy-m33-5g-blue-128gb-storage-8gb-ram-sm-m336bzbrins/buy/"
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
-    product_price = soup.find("span", class_="pd-option-selector__sub-text")
+    product_price = soup.find_all("span", class_="pd-option-selector__sub-text")
+    product_price = (product_price[1].text.strip().split())[-1]  if len(product_price) > 1 else (product_price[0].text.strip().split())[-1]
     product_name = soup.find("h2", class_="pd-info__title")
     result = f"""Product Name: {product_name.text.strip()},
-    Product Price: {(product_price.text.strip().split())[-1]},
+    Product Price: {(product_price)},
     Product Avaialblity : in stock"""
     print(result)
 
