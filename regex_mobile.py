a365a102ae2bc889cdafae9baf8c090fca27d37a
ShubhChaturvedi <chaturvedishubh650@gmail.com>
2023-05-31 11:34:41 +0530

    DEV: regex

---
+++ regex_mobile.py
@@ -0,0 +1,26 @@
+MOBILE_REGEX = {
+    "model": r"[\w\s]+",
+    "color": r"[\w\s]+",
+    "ram": r"\d+GB",
+    "storage": r"\d+GB|\d+GB ROM|\d+ GB",
+    "processor": r"[\w\s]+ Processor|\d+GHz \w+[\s-]?core [\w\s]+ Processor",
+    "battery": r"\d+mAh Battery",
+    "charger": r"No Charger Variant|Charger Included",
+    "display": r"FHD\+ Display|\d+MP[\s\w]+ Display|sAMOLED\+ \d+Hz",
+    "camera": r"\d+MP[\s\w]+ Camera|\d+MP[\s\w]+ Triple Camera|\d+MP[\s\w]+ Dual Camera",
+    "sim": r"Single SIM|Dual SIM",
+    "fm_radio": r"Wireless FM Radio",
+    "camera_type": r"Rear VGA Camera|\d+ MP Camera",
+    "screen_size": r"\d+(\.\d+)? Inch Screen",
+    "memory": r"\d+ GB Free Memory",
+    "refresh_rate": r"\d+Hz Refresh Rate",
+    "expandable_ram": r"\d+GB Expandable RAM",
+    "fast_charging": r"\d+W Pro Fast Charging|\d+W Charger Included",
+    "in_segment": r"Best in-Segment \d+Hz Refresh Rate",
+    "snapdragon": r"Snapdragon \d+[\s\w]+ Processor",
+    "snapdragon_gen": r"Snapdragon \d+ Gen \d+",
+    "dimensity": r"Dimensity \d+ 5G",
+}
+
+
+
